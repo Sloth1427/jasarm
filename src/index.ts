a9247f8831a4ac2edd64fbe0151d00527dba5848
Jacob Reich <jake@Jacobs-MacBook-Pro.local>
2024-06-30 07:17:11 +0100

    update descriptions

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,17 @@ const program = new Command();
 
 program
   .name("jasarm")
-  .description(
-    "Combines JSON files in a given format in to one JSON file, saving the result to a new JSON file"
-  )
+  .description("Manipulate json files from the command line.")
   .version("0.1");
 
 program
   .command("combine")
-  .description("Here is a description")
+  .description(
+    "Combines json files of the format specified in the task in to a single json file, with metadata."
+  )
   .argument("<sring>", "src path")
   .action((path, _options) => {
     combineJSON(path);
   });
 
-program.parse(); // what does this do?!
+program.parse();
